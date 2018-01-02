9403baa153768fa8c5b4aaba8b10839d67fc80fe
Sugarrrrrrrrrr <Sugarrrrrrrrrr@outlook.com>
2018-01-02 08:36:36 +0800

    20171229

--- mavlink_translate/readPcap.py
+++ mavlink_translate/readPcap.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
                 sys.path.append(line)
                 from parse import mavutil
 
-    file_name = 'mission_message_20171229_02'
+    file_name = 'mission_message_20171229_04'
 
     mf = mavutil.mavlink_connection(file_name + '.pcap', ip_list=['192.168.4.1'])
     
